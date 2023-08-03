98ccfa66871ceb274375aa21b1fa1e712b4a8f21
konohaduku <129601109+konohaduku@users.noreply.github.com>
2023-08-03 13:43:29 +0000

    [java4]

--- javava/practice.java
+++ javava/practice.java
@@ -1,5 +1,9 @@
 class Practice {
   public static void main (String[] args) {
     System.out.println(616 * 616);
+    System.out.println("合計金額");
+    int x;
+    x=6;
+    System.out.println(x*x*15);
   }
 }

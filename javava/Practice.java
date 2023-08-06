152c384e3918989db17cab286d1414d4152cc01e
konohaduku <129601109+konohaduku@users.noreply.github.com>
2023-08-06 21:24:46 +0000

    [java6]

--- javava/Practice.java
+++ javava/Practice.java
@@ -5,13 +5,17 @@ class Practice {
     // int c = a * b;
     // System.out.println("縦幅3横幅5の長方形の面積は," + c);
 
-    // boolean anser = true;
-    // char name = '駆';
-    // double pi = 3.14;
-    // long water = 314259265853979L;
-    // String msg = "ミナトの攻撃！敵に１５ポイントのダメージを与えた！";
+    boolean anser = true;
+    char name = '駆';
+    double pi = 3.14;
+    long water = 314259265853979L;
+    String msg = "ミナトの攻撃！敵に１５ポイントのダメージを与えた！";
 
-    // System.out.println("トニー・スターク\"私がアイアンマンだ\" ");
+    System.out.println("トニー・スターク\"私がアイアンマンだ\" ");
     
+    int a;
+    a = 100;
+    a++;
+    System.out.println(a);
   }
 }

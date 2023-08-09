1895cea50317eca50b6c0191cc38edd7a5cad7bd
konohaduku <129601109+konohaduku@users.noreply.github.com>
2023-08-09 10:32:46 +0000

    [java8]

--- javava/Practice.java
+++ javava/Practice.java
@@ -11,18 +11,36 @@ class Practice {
     // long water = 314259265853979L;
     // String msg = "ミナトの攻撃！敵に１５ポイントのダメージを与えた！";
 
+    //オペランド
     // System.out.println("トニー・スターク\"私がアイアンマンだ\" ");
     
+    //インクリメント・デクリメント演算子
     // int a;
     // a = 100;
     // a++;
     // System.out.println(a);
     
-    float f =3;
-    double d = f;
-    System.out.println(f);
-    System.out.println(d);
-    int age = (int)3.5;
-    System.out.println(age);
+    //値より大きな変数に代入
+    // float f =3;
+    // double d = f;
+    // System.out.println(f);
+    // System.out.println(d);
+    
+    //強制的な型変換
+    // int age = (int)3.5;
+    // System.out.println(age);
+    
+    //異なる型の演算
+    // double d = 8.5 / 2;
+    // long l = 5 + 2L;
+    // System.out.println(d);
+    // System.out.println(l);
+    
+    //改行しない命令
+    // String name = "しがらき";
+    // System.out.print("私の苗字は");
+    // System.out.print(name);
+    
+    
   }
 }

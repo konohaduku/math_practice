e8c4f52fb77a190d37fd534f2982f5d3b4677229
konohaduku <129601109+konohaduku@users.noreply.github.com>
2023-08-09 14:14:29 +0000

    [java9]

--- javava/Practice.java
+++ javava/Practice.java
@@ -13,34 +13,49 @@ class Practice {
 
     //オペランド
     // System.out.println("トニー・スターク\"私がアイアンマンだ\" ");
-    
+
     //インクリメント・デクリメント演算子
     // int a;
     // a = 100;
     // a++;
     // System.out.println(a);
-    
+
     //値より大きな変数に代入
     // float f =3;
     // double d = f;
     // System.out.println(f);
     // System.out.println(d);
-    
+
     //強制的な型変換
     // int age = (int)3.5;
     // System.out.println(age);
-    
+
     //異なる型の演算
     // double d = 8.5 / 2;
     // long l = 5 + 2L;
     // System.out.println(d);
     // System.out.println(l);
-    
+
     //改行しない命令
     // String name = "しがらき";
     // System.out.print("私の苗字は");
     // System.out.print(name);
+
+    //比較と最大数値
+    // int a = 5;
+    // int b = 2;
+    // int c = Math.max(a,b);
+    // System.out.println("比較：" + a + "と" + b + "で大きい方は" + c);
     
+    //ランダムな数の生成
+    // int r = new java.util.Random().nextInt(90);
+    // System.out.println("今回は" + r + "位だった");
     
+    //キーボードから入力を受け付ける
+    System.out.println("プレイヤーネームを入力してください");
+    String name = new java.util.Scanner(System.in).nextLine();
+    System.out.println("プレイヤーの年齢を入力してください");
+    int age = new java.util.Scanner(System.in).nextInt();
+    System.out.println(name + "さんは" + age + "歳で登録されました");
   }
 }

e0408610421a37fba686b98b047488c0062fa96c
konohaduku <129601109+konohaduku@users.noreply.github.com>
2023-09-01 14:47:23 +0000

    [java26]

---
+++ javava/Practice2.java
@@ -0,0 +1,12 @@
+class Practice2 {
+  public static void main (String[] args) {
+    //メソッドの利用
+    //・見通しが良くなる・作業効率が上がる
+    System.out.println("メソッド呼び出し開始"); //mainメソッド
+    hello(); //helloメソッドを呼び出す
+    System.out.println("メソッド呼び出し完了");
+  }
+ public static void hello() {
+   System.out.println("湊、こんにちは"); //helloメソッド
+ }
+}

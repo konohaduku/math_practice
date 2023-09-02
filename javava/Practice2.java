a50c369f6f7578e8f7aca1c0712aa202e57bed00
konohaduku <129601109+konohaduku@users.noreply.github.com>
2023-09-02 15:38:25 +0000

    [java27]

--- javava/Practice2.java
+++ javava/Practice2.java
@@ -1,12 +1,26 @@
 class Practice2 {
+  
+  public static void methodB() {
+    System.out.println("methodA");
+    methodB();
+  }
+  
+  public static void methodA() {
+    System.out.println("methodB");
+  }
+  
   public static void main (String[] args) {
+     methodA();
+  
     //メソッドの利用
     //・見通しが良くなる・作業効率が上がる
-    System.out.println("メソッド呼び出し開始"); //mainメソッド
-    hello(); //helloメソッドを呼び出す
-    System.out.println("メソッド呼び出し完了");
-  }
- public static void hello() {
-   System.out.println("湊、こんにちは"); //helloメソッド
+//     System.out.println("メソッド呼び出し開始"); //mainメソッド
+//     hello(); //helloメソッドを呼び出す
+//     System.out.println("メソッド呼び出し完了");
+//   }
+// public static void hello() {
+//   System.out.println("湊、こんにちは"); //helloメソッド
+
+   
  }
 }
